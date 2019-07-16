f2c88212d99cdb2e867ec75acf73ac6cf1a863c4
mohamedhaz <48091311+mohamedhaz@users.noreply.github.com>
2019-07-17 00:00:57 +0100

    first commit

---
+++ jquerry.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $( ".first-ul li" ).draggable({
+      heper : "clone"
+    });
+  //   $(".item2").droppable({
+  //      drop: function(event, ui){
+  //        $(".contry").append(ui.draggable);
+  //      }
+  //   });
+  //   $(".item3").droppable({
+  //     drop: function(event, ui){
+  //       $(".city").append(ui.draggable);
+  //     }
+  //  });
+  });
